d2a466c3d76f8fb494854c62844d771f6e33c582
Ryzen <jakubsobota535@gmail.com>
2024-07-27 02:52:01 +0200

    2 columns

--- src/views/activityHistory/HistoryView.vue
+++ src/views/activityHistory/HistoryView.vue
@@ -1,13 +1,13 @@
 <template>
     <VRow justify="center" noGutters>
-        <VCol cols="10" sm="8" md="10" lg="8" class="pt-0 pt-md-2">
+        <VCol cols="10" sm="8" md="10" lg="8"  class="pt-0 pt-md-2">
             <HistoryPanelFilter @filterApplied="handleFilterApplied"></HistoryPanelFilter>
         </VCol>
-        <VCol cols="12" class="pt-md-0 px-sm-6 px-md-8 px-lg-10">
-            <VRow justify="center">
-                <VCol cols="12" lg="6" class="py-0 py-md-2 d-flex flex-column align-center align-lg-end">
-	                <div v-for="groupedRecord in groupedRecords">
-		                <div>{{formatLocalized(groupedRecord.date,'L')}}</div>
+        <VCol cols="12" class="pt-md-0 px-sm-2 px-md-4 px-lg-6">
+            <VRow justify="start" class="my-2">
+                <VCol cols="12" lg="6" class="py-0 py-md-2 d-flex flex-column">
+	                <div class="w-100" v-for="groupedRecord in groupedRecords?.slice(0, groupedRecords.length / 2 + 1)">
+		                <div class="w-100 bg-blue-grey rounded text-center">{{formatLocalized(groupedRecord.date,'L')}}</div>
 		                <HistoryRecordItem
 			                class="my-2 my-md-3 w-100"
 			                :record="record"
@@ -16,6 +16,17 @@
 		                ></HistoryRecordItem>
 	                </div>
                 </VCol>
+	            <VCol cols="12" lg="6" class="py-0 py-md-2 d-flex flex-column">
+		            <div class="w-100" v-for="groupedRecord in groupedRecords?.slice(groupedRecords.length / 2 + 1)">
+			            <div class="w-100 bg-blue-grey rounded text-center">{{formatLocalized(groupedRecord.date,'L')}}</div>
+			            <HistoryRecordItem
+				            class="my-2 my-md-3 w-100"
+				            :record="record"
+				            v-for="record in groupedRecord.historyList?.slice(0, groupedRecord.historyList.length / 2 + 1)"
+				            :key="record.id"
+			            ></HistoryRecordItem>
+		            </div>
+	            </VCol>
             </VRow>
         </VCol>
     </VRow>
